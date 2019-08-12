f55a3eb8c546e7b7b0916078ce4974ce0eadf1f7
Lon <gavinlondee@gmail.com>
2019-08-12 14:47:08 +0200

    Initial commit

---
+++ EmployeeCRUDApplicationExample/Program.cs
@@ -0,0 +1,70 @@
+﻿using EmployeeComponent;
+using EmployeeComponent.Data;
+using EmployeeComponent.Views;
+using System;
+using System.Globalization;
+
+namespace EmployeeCRUDApplicationExample
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            bool endApplication = false;
+
+            Employees employees = new Employees();
+
+            EmployeeRecordsView employeeRecordsView = EmployeeObjectFactory.EmployeeRecordsViewObject(employees);
+
+
+            while (!endApplication)
+            {
+                Console.Clear();
+
+                Console.WriteLine(EmployeeCommonOutputText.GetApplicationHeading());
+
+                employeeRecordsView.RunRecordsView();
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Console.WriteLine(EmployeeCommonOutputText.GetInstructions());
+
+                ConsoleKey instructionKey = Console.ReadKey().Key;
+
+                switch (instructionKey)
+                {
+                    case ConsoleKey.C:
+                        EmployeeCreateView employeeCreateView = EmployeeObjectFactory.EmployeeCreateViewObject(employees);
+                        employeeCreateView.RunCreateView();
+                        break;
+                    case ConsoleKey.R:
+                        EmployeeReadView employeeReadView = EmployeeObjectFactory.EmployeeReadViewObject(employees);
+                        employeeReadView.RunReadView();
+                        break;
+                    case ConsoleKey.U:
+                        Console.Clear();
+                        Console.WriteLine("Update functionality not yet implemented.");
+                        Console.ReadKey();
+                        break;
+                    case ConsoleKey.D:
+                        Console.Clear();
+                        Console.WriteLine("Delete functionality not yet implemented.");
+                        Console.ReadKey();
+                        break;
+                    default:
+
+                        endApplication = true;
+                        break;
+                }
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ EmployeeComponent/EmployeeObjectFactory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EmployeeComponent.Data;
+using EmployeeComponent.Views;
+
+namespace EmployeeComponent
+{   
+    public static class EmployeeObjectFactory
+    {
+        private static EmployeeCreateView _employeeCreateView = null;
+        private static EmployeeReadView _employeeReadView = null;
+
+        public static Employee CreateNewEmployeeObject(string firstName, string lastName, decimal annualSalary, char gender, bool isManager)
+        {
+            return new Employee
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                AnnualSalary = annualSalary,
+                Gender = gender,
+                IsManger = isManager
+            };
+
+        }
+
+        public static EmployeeRecordsView EmployeeRecordsViewObject(Employees employees)
+        {
+            return new EmployeeRecordsView(employees);
+        }
+
+        public static EmployeeCreateView EmployeeCreateViewObject(Employees employees)
+        {
+            if (_employeeCreateView == null)
+            {
+                _employeeCreateView = new EmployeeCreateView(employees);
+            }
+
+            return _employeeCreateView;
+        }
+        public static EmployeeReadView EmployeeReadViewObject(Employees employees)
+        {
+            if (_employeeReadView == null)
+            {
+                _employeeReadView = new EmployeeReadView(employees);
+            }
+
+            return _employeeReadView;
+        }
+    }
+}
